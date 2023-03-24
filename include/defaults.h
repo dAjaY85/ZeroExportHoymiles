216fd066c6ac304d3f7a2eed479c13ab3ab0ae40
dAjaY85 <AndreasElsesser@gmx.de>
2023-03-24 10:45:04 +0100

    first commit

---
+++ include/defaults.h
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define SERIAL_BAUDRATE 115200
+
+#define APP_HOSTNAME "ZeroExport-%06X"
+
+#define HTTP_PORT 80
+
+// DTU SETTINGS
+#define DTU_IP "192.168.178.xxx"
+#define DTU_USER "admin"
+#define DTU_PASS "openDTU42"
+
+// WIFI
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
